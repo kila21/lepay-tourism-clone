14b83b764b76429e726d4c192b6057828a421929
kila <luka.kilasonia.1@btu.edu.ge>
2024-01-15 20:17:14 +0400

    tourType

--- src/types/JsonTypes/ToursType.ts
+++ src/types/JsonTypes/ToursType.ts
@@ -2,4 +2,16 @@ export interface ToursType {
   name: string;
   place: string;
   img: string;
+  page: {
+    routeName: string;
+    route: string;
+    headline: string;
+    slider: string[];
+    content: contentType[];
+  };
+}
+
+interface contentType {
+  header: string;
+  text: string[];
 }
